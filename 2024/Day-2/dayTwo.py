d00dc75e824190705e27d70d41d631cc20a71d91
Teknik <teknik@TEK-STATION.localdomain>
2025-11-20 15:29:03 -0500

    If this doesn't work I will be so angry

--- 2024/Day-2/dayTwo.py
+++ 2024/Day-2/dayTwo.py
@@ -1,16 +1,30 @@
 import os
 
 def checkThings(inputList: list[int]):
-    errCount: int = 0
     returnVal: bool = False
+    if all(x < y for x, y in zip(inputList, inputList[1:])):
+        returnVal = True
+    elif all(x > y for x, y in zip(inputList, inputList[1:])):
+        returnVal = True
     for j, t in zip(inputList, inputList[1:]):
         absol: int = abs(j-t)
         if not (absol in range(1, 4)):
-            errCount += 1
-    if errCount <= 1:
-        returnVal = True
-    else:
-        print(errCount)
+            returnVal = False
+    return returnVal
+
+def brute(input: list[int]):
+    tNewList: list[int] = []
+    i: int = 0
+    returnVal: bool = False
+    for i in range(len(input)):
+        tNewList = []
+        j: int = 0
+        for j in input:
+            tNewList.append(j)
+        tNewList.pop(i)
+        if checkThings(tNewList):
+            print(input, tNewList)
+            returnVal = True
     return returnVal
 
 def main():
@@ -31,18 +45,12 @@ def main():
         fullLine = list(map(int, data[i].strip().split(" ")))
         fixedData.append(fullLine)
 
-    bwaa: int = 0
     for fDIndex in fixedData:
-        bwaa+= 1
-        #if all(x < y for x, y in zip(fDIndex, fDIndex[1:])):
-            #opType = True
-        #elif all(x > y for x, y in zip(fDIndex, fDIndex[1:])):
-            #opType = True
         opType = checkThings(fDIndex)
+        if not opType:
+            opType = brute(fDIndex)
         if opType:
             count += 1
-        else:
-            print("Error at:", bwaa)
             
     print(count)
 
